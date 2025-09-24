38b7d0b48a5fd5d6eb688c1a66aff6ade6e1439d
Marcelo Sanchez <39056094+msan72@users.noreply.github.com>
2025-09-24 23:55:48 +0000

    1commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+// Arrays
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+// Function Math.random
+function generateExcuse() {
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+  return excuse;
+}
 
+// html
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
+
+  //button
+  document.getElementById("newExcuseBtn").addEventListener("click", function() {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+  });
 };
